64d904374d4d121008ecfe8832b9c0c8730765b7
Rani Patra <raninh99@gmail.com>
2021-07-10 11:48:03 +0530

    added grid service

--- projects/angular-material-app/src/app/grid/grid.component.ts
+++ projects/angular-material-app/src/app/grid/grid.component.ts
@@ -10,7 +10,7 @@ import {SelectionModel} from '@angular/cdk/collections';
 })
 export class GridComponent implements AfterViewInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['application', 'category', 'businessProcess', 'topic', 'eventCode'];
   columnsToDisplay: string[] = this.displayedColumns.slice();
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
@@ -22,73 +22,45 @@ export class GridComponent implements AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
-  
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    if (this.isAllSelected()) {
-      this.selection.clear();
-      return;
-    }
-
-    this.selection.select(...this.dataSource.data);
-  }
-
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  addColumn() {
-    const randomColumn = Math.floor(Math.random() * this.displayedColumns.length);
-    this.columnsToDisplay.push(this.displayedColumns[randomColumn]);
-  }
-
 }
 
 export interface PeriodicElement {
-  name: string;
+  application: string;
   position: number;
-  weight: number;
-  symbol: string;
+  category: string;
+  businessProcess: string;
+  topic : string;
+  eventCode: number;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+  // {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', topic: 'Eng', eventCode: 45},
+  // {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', topic: 'hin', eventCode: 48},
+  // {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  // {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  // {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  // {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  // {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  // {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  // {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  // {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+  // {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+  // {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+  // {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+  // {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+  // {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+  // {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+  // {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+  // {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+  // {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
 ];
 
 
